71e40e97dfd928a363bc87988d2c85c62bd37e4e
stephaniebrandao <salbuquerque.md@gmail.com>
2026-04-27 22:13:21 +0100

    Enhance Card component: add link prop and update button structure

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,20 +1,23 @@
-import styles from "./card.module.css"
+import styles from "./card.module.css";
 
 type CardCharacteristics = {
-    image: string,
-    title: string,
-    subtitle: string
-}
+  image: string;
+  title: string;
+  subtitle: string;
+  link: string;
+};
 
-function Card ({image, title, subtitle}: CardCharacteristics) {
-    return (
-        <div className={styles.container}>
-            <img src={image} alt="" />
-            <h3>{title}</h3>
-            <p>{subtitle}</p>
-            <button>View on GitHub</button>
-        </div>
-    )
+function Card({ image, title, subtitle, link }: CardCharacteristics) {
+  return (
+    <div className={styles.container}>
+      <img src={image} alt="" />
+      <h3>{title}</h3>
+      <p>{subtitle}</p>
+      <a href={link} target="_blank">
+        <button>View on GitHub</button>
+      </a>
+    </div>
+  );
 }
 
 export default Card;
